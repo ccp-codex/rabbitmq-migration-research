a0ad5ee990b088f023d9b8f279089ea638885f86
CCP Codex <31438281+ccp-codex@users.noreply.github.com>
2020-02-03 10:26:43 +0000

    no b

--- testapp/src/main.rs
+++ testapp/src/main.rs
@@ -24,19 +24,19 @@ async fn main() -> Result<()> {
     .await
     .expect("can't open consume channel on node A");
 
-  info!("connecting to node B");
-  let node_b = "amqp://guest:guest@localhost:5002/fed-test";
-  let conn_b = Connection::connect(node_b, ConnectionProperties::default())
-    .await
-    .expect("can't connect to node B");
-  let pub_channel_b = conn_b
-    .create_channel()
-    .await
-    .expect("can't open publish channel on node B");
-  let con_channel_b = conn_b
-    .create_channel()
-    .await
-    .expect("can't open consume channel on node B");
+  // info!("connecting to node B");
+  // let node_b = "amqp://guest:guest@localhost:5002/fed-test";
+  // let conn_b = Connection::connect(node_b, ConnectionProperties::default())
+  //   .await
+  //   .expect("can't connect to node B");
+  // let pub_channel_b = conn_b
+  //   .create_channel()
+  //   .await
+  //   .expect("can't open publish channel on node B");
+  // let con_channel_b = conn_b
+  //   .create_channel()
+  //   .await
+  //   .expect("can't open consume channel on node B");
 
   // expected to use same exchange and queue names on both nodes
   let exchange_name = "fed-test-exchange";
@@ -56,18 +56,18 @@ async fn main() -> Result<()> {
     .wait()
     .expect("can't declare exchange on node A");
 
-  pub_channel_b
-    .exchange_declare(
-      exchange_name,
-      ExchangeKind::Topic,
-      ExchangeDeclareOptions {
-        durable: true,
-        ..Default::default()
-      },
-      FieldTable::default(),
-    )
-    .wait()
-    .expect("can't declare exchange on node B");
+  // pub_channel_b
+  //   .exchange_declare(
+  //     exchange_name,
+  //     ExchangeKind::Topic,
+  //     ExchangeDeclareOptions {
+  //       durable: true,
+  //       ..Default::default()
+  //     },
+  //     FieldTable::default(),
+  //   )
+  //   .wait()
+  //   .expect("can't declare exchange on node B");
 
   // declare queues and bindings
   let queue_a = con_channel_a
@@ -94,29 +94,29 @@ async fn main() -> Result<()> {
     .wait()
     .expect("can't bind queue on node A");
 
-  let queue_b = con_channel_b
-    .queue_declare(
-      queue_name,
-      QueueDeclareOptions {
-        exclusive: true,
-        auto_delete: true,
-        ..Default::default()
-      },
-      FieldTable::default(),
-    )
-    .await
-    .expect("can't declare queue on node B");
+  // let queue_b = con_channel_b
+  //   .queue_declare(
+  //     queue_name,
+  //     QueueDeclareOptions {
+  //       exclusive: true,
+  //       auto_delete: true,
+  //       ..Default::default()
+  //     },
+  //     FieldTable::default(),
+  //   )
+  //   .await
+  //   .expect("can't declare queue on node B");
 
-  con_channel_b
-    .queue_bind(
-      queue_name,
-      exchange_name,
-      "#",
-      QueueBindOptions::default(),
-      FieldTable::default(),
-    )
-    .wait()
-    .expect("can't bind queue on node B");
+  // con_channel_b
+  //   .queue_bind(
+  //     queue_name,
+  //     exchange_name,
+  //     "#",
+  //     QueueBindOptions::default(),
+  //     FieldTable::default(),
+  //   )
+  //   .wait()
+  //   .expect("can't bind queue on node B");
 
   // start consumers
   let consumer_a = con_channel_a
@@ -144,30 +144,30 @@ async fn main() -> Result<()> {
       .await
   });
 
-  let consumer_b = con_channel_b
-    .clone()
-    .basic_consume(
-      &queue_b,
-      "consumer-b",
-      BasicConsumeOptions::default(),
-      FieldTable::default(),
-    )
-    .await
-    .expect("can't consume from node B");
+  // let consumer_b = con_channel_b
+  //   .clone()
+  //   .basic_consume(
+  //     &queue_b,
+  //     "consumer-b",
+  //     BasicConsumeOptions::default(),
+  //     FieldTable::default(),
+  //   )
+  //   .await
+  //   .expect("can't consume from node B");
 
-  tokio::spawn(async move {
-    info!("consuming from node B");
+  // tokio::spawn(async move {
+  //   info!("consuming from node B");
 
-    consumer_b
-      .for_each(move |delivery| {
-        let msg = delivery.expect("failed to receive from node B");
-        info!("-> B, received: {}", String::from_utf8(msg.data).unwrap());
-        con_channel_b
-          .basic_ack(msg.delivery_tag, BasicAckOptions::default())
-          .map(|_| ())
-      })
-      .await
-  });
+  //   consumer_b
+  //     .for_each(move |delivery| {
+  //       let msg = delivery.expect("failed to receive from node B");
+  //       info!("-> B, received: {}", String::from_utf8(msg.data).unwrap());
+  //       con_channel_b
+  //         .basic_ack(msg.delivery_tag, BasicAckOptions::default())
+  //         .map(|_| ())
+  //     })
+  //     .await
+  // });
 
   // start publishing on both nodes
   tokio::spawn(async move {
@@ -194,24 +194,24 @@ async fn main() -> Result<()> {
     }
   });
 
-  info!("publishing from node B");
-  let mut cnt = 10;
+  // info!("publishing from node B");
+  // let mut cnt = 10;
 
-  loop {
-    delay_for(Duration::from_millis(5000)).await;
+  // loop {
+  //   delay_for(Duration::from_millis(5000)).await;
 
-    let payload_b = format!("Msg {} from node B", cnt).into_bytes();
-    cnt += 1;
+  //   let payload_b = format!("Msg {} from node B", cnt).into_bytes();
+  //   cnt += 1;
 
-    pub_channel_b
-      .basic_publish(
-        exchange_name,
-        "route.b",
-        BasicPublishOptions::default(),
-        payload_b.to_vec(),
-        BasicProperties::default(),
-      )
-      .await
-      .unwrap();
-  }
+  //   pub_channel_b
+  //     .basic_publish(
+  //       exchange_name,
+  //       "route.b",
+  //       BasicPublishOptions::default(),
+  //       payload_b.to_vec(),
+  //       BasicProperties::default(),
+  //     )
+  //     .await
+  //     .unwrap();
+  // }
 }
